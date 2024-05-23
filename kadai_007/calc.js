6f300472209273a40e802e53cb1eff82b2ebfb7c
shiro0710 <79732320+shiroto0710@users.noreply.github.com>
2024-05-23 23:38:36 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,14 @@
+const num = Math.floor(Math.random() * 30);
+
+console.log("numは" + num);
+
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+    console.log("3の倍数です");
+} else if (num % 5 === 0) {
+    console.log("5の倍数です")
+} else {
+    console.log(num);
+}
+
