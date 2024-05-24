0d8b1d0b252d56c5ba68c5797866d97913490d23
shiro0710 <79732320+shiroto0710@users.noreply.github.com>
2024-05-24 17:18:39 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,10 @@
+
+const getDate = new Date();
+
+const year = getDate.getFullYear();
+
+const month = getDate.getMonth() + 1;
+
+const day = getDate.getDate();
+
+console.log(year + '年' + month + '月' + day + '日')
